a0a8f93e85db685bd41ff5284ddac02cb8842577
domingosdias1010@gmail.com <domingosdias1010@gmail.com>
2026-04-16 20:18:15 +0100

    update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,17 @@
 const CACHE_NAME = 'portfolio-cache-v1';
 
-const assets = ['./', './index.html', './css/styles.css','./css/slide.css','./css/bot.js','./js/scripts.js','./js/link.js','./js/slide.js','./js/bot/knowledgeBase.js','./js/bot/bot.js']; // adicione seus arquivos principais
+const assets = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './css/slide.css',
+  './css/bot.js',
+  './js/scripts.js',
+  './js/link.js',
+  './js/slide.js',
+  './js/bot/knowledgeBase.js',
+  './js/bot/bot.js'
+]; // adicione seus arquivos principais
 
 self.addEventListener('install', e => {
   e.waitUntil(
